fa88bb76dc5cc5a9a0838123c8a9d815aeae49f1
Crisbeth <crisbeth_adames@estudante.sesisenai.org.br>
2025-11-12 16:36:03 -0300

    Atulizaçõ do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Header from "./componentes/Header";
-import Footer from "./componentes/Footer";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
 import Home from "./paginas/Home";
 import Sobre from "./paginas/Sobre";
 import "./style.css";
-import "./header.css";
+import "./header/header.css";
+import "./footer/footer.css";
+
+
+
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Header />
-
-      
 
         <Routes>
           <Route path="/" element={<Home />} />
