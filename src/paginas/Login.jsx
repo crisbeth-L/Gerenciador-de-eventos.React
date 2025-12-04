2569b20bb6d0b7d5289d5927e449bcff9e762a15
CRISBETH BETZABETH LEON ADAMES <crisbeth_adames@estudante.sesisenai.org.br>
2025-12-04 14:17:07 -0300

    Atualização Dia 04/12/2025

--- src/paginas/Login.jsx
+++ src/paginas/Login.jsx
@@ -12,32 +12,38 @@ function Login({ setLogado }) {
 
     const emailsPermitidos = ["Crisbeth@gmail.com", "Catherine@gmail.com"];
 
-    if (emailsPermitidos.includes(email) && senha === "123456") {//confirmação dos usuários3
-      setLogado(true); // 
-      navigate("/home"); // Faz com que depois do Login seja aberto o Home 
+    if (emailsPermitidos.includes(email) && senha === "123456") {
+      setLogado(true);
+      navigate("/home");
     } else {
       alert("Email ou senha inválidos!");
     }
   };
 
   return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Digite seu email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Digite sua senha"
-          value={senha}
-          onChange={(e) => setSenha(e.target.value)}
-        />
-        <button type="submit">Entrar</button>
-      </form>
+    <div className="login-wrapper">
+      {/* Lado da imagem */}
+      <div className="login-image"></div>
+
+      {/* Lado do formulário */}
+      <div className="login-container">
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Digite sua senha"
+            value={senha}
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          <button type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
